e5347f4221b035c0d4218b792b38a38e5a90e192
Grady <yhwei876@gmail.com>
2019-08-13 20:15:07 +0800

    clean up and add Installspoon.

--- init.lua
+++ init.lua
@@ -1,15 +1,2 @@
-require "preload"
-
-local modal = require "modal"
-
-hs.hints.style           = "vimperator"
-hs.hints.showTitleThresh = 4
-hs.hints.titleMaxSize    = 10
-hs.hints.fontSize        = 30
-
-require("windows").addState(modal)
-
-local stateMachine = modal.createMachine()
-stateMachine:toMain()
 
 hs.alert.show("Hammerspoon config loaded")
