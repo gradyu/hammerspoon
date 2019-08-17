e935931a1bd9b3f9ee1b967b8cfd7bbcf5439205
Grady <yhwei876@gmail.com>
2019-08-17 22:14:45 +0800

    countdown add cancel key.

--- module/countdown.lua
+++ module/countdown.lua
@@ -8,16 +8,20 @@ m.map = {}
 local majorkey = g_count_major_keys or {}
 
 if hs.spoons.isLoaded("CountDown") then
-  local singleKey = spoon.RecursiveBinder.singleKey
-  m.key = singleKey(majorkey[1] or 'c', majorkey[2] or "countdown")
+  local utils = require("utils")
+  m.key = utils.singleKey(majorkey[1] or 'c', majorkey[2] or "countdown")
   m.map[{{}, g_count_resume_pause_key[1], g_count_resume_pause_key[2]}] = function ()
     spoon.CountDown:pauseOrResume()
   end
-  m.map[{{}, "return", "30 minutes"}] = function ()
+  m.map[{{}, 'return', "30 minutes"}] = function ()
+    print("countdown 30")
     spoon.CountDown:startFor(30)
   end
+  m.map[{{}, '0', "cancel"}] = function ()
+	spoon.CountDown:setProgress(1.0)
+  end
   for _, v in ipairs(g_count_keymap) do
-    m.map[singleKey(v.key, v.desc)] = function ()
+    m.map[utils.singleKey(v.key, v.desc)] = function ()
       spoon.CountDown:startFor(v.count)
     end
   end
